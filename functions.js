d20375424ff231385317f2452ff62126251cf2f0
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2021-09-02 21:25:26 -0300

    modal about

--- functions.js
+++ functions.js
@@ -139,6 +139,7 @@ function redirect(page, modal, type) {
 
 function closeModal() {
     modal.style.display = "none";
+    aboutModal.style.display = "none";
 }
 
 function openModal() {
@@ -451,26 +452,48 @@ function readMapFile() {
 
 var modal = document.getElementById("myModal");
 
+var aboutModal = document.getElementById("aboutModal");
+
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+// Get the button that opens the modal
+var aboutBtn = document.getElementById("aboutBtn");
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[1];
+
+var span4 = document.getElementsByClassName("close")[2];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 };
 
+// When the user clicks on the button, open the modal
+aboutBtn.onclick = function() {
+    aboutModal.style.display = "block";
+};
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    aboutModal.style.display = "none";
+    console.log("close span")
+};
+
+span4.onclick = function() {
+    modal.style.display = "none";
+    aboutModal.style.display = "none";
+    console.log("close span")
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        aboutModal.style.display = "none";
+        console.log("close window")
     }
 };
 
